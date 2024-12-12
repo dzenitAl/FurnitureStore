347c7ed563e9ffa5a66221639b3d1cf1cb0090b5
dzenitAl <dzenita.alibegic@edu.fit.ba>
2024-12-12 18:40:14 +0100

    Update home screen

--- FurnitureStore/UI/furniturestore_mobile/lib/screens/home/home_screen.dart
+++ FurnitureStore/UI/furniturestore_mobile/lib/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:furniturestore_mobile/models/promotion/promotion.dart';
+import 'package:furniturestore_mobile/providers/promotion_provider.dart';
+import 'package:furniturestore_mobile/screens/product/product_list_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:furniturestore_mobile/providers/category_provider.dart';
 import 'package:furniturestore_mobile/models/category/category.dart';
@@ -14,19 +17,25 @@ class HomePageScreen extends StatefulWidget {
 class _HomePageScreenState extends State<HomePageScreen> {
   late CategoryProvider _categoryProvider;
   List<CategoryModel>? categories;
+  late PromotionProvider _promotionProvider;
+  List<PromotionModel>? promotions;
 
   @override
   void initState() {
     super.initState();
     _categoryProvider = context.read<CategoryProvider>();
+    _promotionProvider = context.read<PromotionProvider>();
+
     _loadData();
   }
 
   Future<void> _loadData() async {
     try {
       var categoriesData = await _categoryProvider.get('');
+      var promotionsData = await _promotionProvider.get('');
       setState(() {
         categories = categoriesData.result;
+        promotions = promotionsData.result;
       });
     } catch (e) {
       print("Error loading data: $e");
@@ -43,15 +52,7 @@ class _HomePageScreenState extends State<HomePageScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                "Welcome to the Furniture Store!",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF1D3557),
-                ),
-              ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 10),
               const Text(
                 "Kategorije",
                 style: TextStyle(
@@ -71,34 +72,59 @@ class _HomePageScreenState extends State<HomePageScreen> {
                           .toList()
                       : [
                           _buildCategoryCard(
-                              "Loading...", Icons.hourglass_empty),
+                              "Učitavanje...", Icons.hourglass_empty),
                         ],
                 ),
               ),
               const SizedBox(height: 20),
               const Text(
-                "Popular Products",
+                "Promocije",
                 style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               const SizedBox(height: 10),
-              GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 0.75,
+              SizedBox(
+                height: 120,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: promotions != null
+                      ? promotions!
+                          .map((promotion) =>
+                              _buildPromotionCard(promotion, context))
+                          .toList()
+                      : [
+                          _buildPromotionCard(
+                              PromotionModel(heading: "Loading..."), context),
+                        ],
                 ),
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return _buildPopularProductCard(
-                      "Product $index", "\$${100 + index * 50}");
-                },
               ),
+              const SizedBox(height: 20),
+
+              // const Text(
+              //   "Popularni proizvodi",
+              //   style: TextStyle(
+              //     fontSize: 18,
+              //     fontWeight: FontWeight.bold,
+              //   ),
+              // ),
+              // const SizedBox(height: 10),
+              // GridView.builder(
+              //   shrinkWrap: true,
+              //   physics: const NeverScrollableScrollPhysics(),
+              //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              //     crossAxisCount: 2,
+              //     crossAxisSpacing: 10,
+              //     mainAxisSpacing: 10,
+              //     childAspectRatio: 0.75,
+              //   ),
+              //   itemCount: 4,
+              //   itemBuilder: (context, index) {
+              //     return _buildPopularProductCard(
+              //         "Product $index", "\$${100 + index * 50}");
+              //   },
+              // ),
             ],
           ),
         ),
@@ -110,7 +136,7 @@ class _HomePageScreenState extends State<HomePageScreen> {
     return Padding(
       padding: const EdgeInsets.only(right: 10.0),
       child: Container(
-        width: 100,
+        width: 120,
         decoration: BoxDecoration(
           color: const Color(0xFFF7F7F7),
           borderRadius: BorderRadius.circular(10),
@@ -133,6 +159,54 @@ class _HomePageScreenState extends State<HomePageScreen> {
               style: const TextStyle(fontSize: 16),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPromotionCard(PromotionModel promotion, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ProductListScreen(
+                promotionProducts: promotion.products,
+              ),
+            ),
+          );
+        },
+        child: Container(
+          width: 120,
+          decoration: BoxDecoration(
+            color: const Color(0xFFE4F0D0),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.local_offer, size: 40, color: Color(0xFF70BC69)),
+              const SizedBox(height: 10),
+              Text(
+                promotion.heading ?? 'Promocija',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF1D3557),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
